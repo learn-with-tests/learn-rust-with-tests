0808646645cfd5bf1058c951f383de339b743b8d
Chris James <qui666@gmail.com>
2020-05-30 09:01:11 +0100

    optional greeting

--- src/learning/hello_world/bin/hello_world.rs
+++ src/learning/hello_world/bin/hello_world.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("{}", greet(String::from("world")))
+    println!("{}", greet(None))
 }
 
 #[cfg(test)]
@@ -8,10 +8,23 @@ mod tests {
 
     #[test]
     fn test_greet() {
-        assert_eq!("Hello, Chris!", greet(String::from("Chris")))
+        let name = String::from("Chris");
+        assert_eq!("Hello, Chris!", greet(Option::from(name)))
     }
 }
 
-fn greet(name: String) -> String {
-    format!("Hello, {0}!", name)
+fn greet(name: Option<String>) -> String {
+    let default_greet = String::from("world"); //todo: work into constant
+    format!("Hello, {0}!", name.unwrap_or(default_greet))
 }
+
+/* notes
+- Hooray for option back in my life
+- str vs String eugh, https://www.ameyalokare.com/rust/2017/10/12/rust-str-vs-String.html
+- str = immutable, fixed length string in memory
+- String = growable, on heap
+
+You can only ever interact with str as a borrowed type aka &str. This is called a string slice, an immutable view of a string. This is the preferred way to pass strings around, as we shall see.
+
+The only real use case I can think of is if you want to pass a mutable reference to a function that needs to modify the string:
+ */
