0ce73522ab3bdca52e178c876102ebcfc8ee1133
Chris James <qui666@gmail.com>
2020-06-01 10:09:53 +0100

    meh stupid arrays

---
+++ src/learning/arrays/bin/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    println!("{}",sum([1, 1, 1]))
+}
+
+fn sum(numbers: [i32; 3]) -> i32 {
+    let mut total = 0;
+    for (_, &item) in numbers.iter().enumerate() {
+        total = total + item
+    }
+    return total;
+}
+
+fn sum2(numbers: [i32; 3]) -> i32 {
+    // coerce the array into a slice
+    let iter = numbers.iter();
+
+    let enumerate = iter.enumerate();
+    enumerate.fold(0, |sum, val| sum + val)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{sum, sum2};
+
+    #[test]
+    fn test_sum1() {
+        assert_eq!(6, sum([1, 2, 3]));
+    }
+
+    #[test]
+    fn test_sum2() {
+        assert_eq!(6, sum2([1, 2, 3]));
+    }
+}
+
+/*
+notes
+
+use this crate for reduce https://docs.rs/reduce/0.1.2/reduce/
+ */
